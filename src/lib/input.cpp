129ac9416bc3d4c3d1c0278eb5c6febd324d5935
ralvarezdev <86166683+ralvarezdev@users.noreply.github.com>
2023-10-03 20:58:23 -0400

    feat: Added Input Function to Ask [y/N] Questions

---
+++ src/lib/input.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::string;
+
+bool booleanQuestion(string message)
+{
+  string input;
+  char c;
+
+  while (true)
+  {
+    cout << "\n"
+         << message << " [y/N] ";
+    cin >> input;
+
+    c = tolower(input[0]);
+    if (c == 'y')
+    {
+      return true;
+    }
+    else if (c == 'n')
+    {
+      return false;
+    }
+    else
+    {
+      cout << "\t- Wrong Input: It's a Yes/No Question. You must type 'y', 'Y' or 'n', 'N'"
+           << "\n";
+    }
+  }
+}
