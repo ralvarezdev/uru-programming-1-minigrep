f6a5fa299690f6a49d8a67a705c0bb5677859bae
ralvarezdev <86166683+ralvarezdev@users.noreply.github.com>
2023-10-05 10:19:52 -0400

    fix: Added the Untracked Files that were Missing in the Previous Commit

---
+++ src/lib/filesOp.h
@@ -0,0 +1,10 @@
+#include <string>
+
+using std::string;
+
+#ifndef FILES_OP_H
+#define FILES_OP_H
+
+void writeToFile(char *filename, string fileContent, int numberArgs, string invokeCommand);
+
+#endif
