d03b55d8ee2d7ab7b40c0f0b2b4fbdf6b8078fff
ralvarezdev <86166683+ralvarezdev@users.noreply.github.com>
2023-10-05 17:55:48 -0400

    fix: Removed Some Lines Used for Testing that weren't Deleted

--- src/lib/filesOp.h
+++ src/lib/filesOp.h
@@ -5,6 +5,6 @@ using std::string;
 #ifndef FILES_OP_H
 #define FILES_OP_H
 
-void writeToFile(char *filename, string fileContent, int numberArgs, string invokeCommand);
+void writeToFile(char *filename, string fileContent, string invokeCommand);
 
 #endif
